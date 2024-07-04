d47d6ce448c69f815db0a4f98a22cc683a113504
Mansayy <mansi.joshi9789@gmail.com>
2024-07-04 14:51:25 -0400

    MJ: Backend Code

---
+++ book-network/src/test/java/com/mansi/book/BookNetworkApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mansi.book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookNetworkApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
